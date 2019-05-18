4cda1481a27b16fcd13407cb0bf6bf178ab95d02
Paweł Bucior <pawel121111@gmail.com>
2019-05-18 11:58:58 +0200

    New endpoint
    I added get garage

---
+++ src/main/java/w58984/carrental/model/DTO/Garage/GarageDTO.java
@@ -0,0 +1,18 @@
+package w58984.carrental.model.DTO.Garage;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class GarageDTO {
+
+    private String name;
+
+    private String address;
+
+}
